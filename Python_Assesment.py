6a18dc5270697d4d3c52c9b0876b5f992c805b63
TylerTalbot <tt23370@mbc.school.nz>
2026-06-26 10:30:34 +1200

    I added the name input for the user

--- Python_Assesment.py
+++ Python_Assesment.py
@@ -1,10 +1,21 @@
+#------------------------------------------------------------------------------------------------------------------------------------------------------
+# This is place for all my imports
+
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import ttk
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------
 
 root = tk.Tk()
-root.geometry("600x600")
+root.geometry("400x400")
+root.grid_columnconfigure(2, weight=1)
 root.title("Party Hire Shop")
 root.configure(bg='grey')
+customers = []
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------
+# This is my space for all of my lists and constants
 
 PRICING = [
     #Furniture
@@ -19,9 +30,22 @@ PRICING = [
     {"Bouncy Castles": 20.00, "Clowns": 15.00, "Magicians": 15.00}
 ]
 
-MAX_STOCK_FOR_EACH_PRODUCT_AVAILABLE = 100
+MAX_STOCK_FOR_EACH_PRODUCT_AVAILABLE = 500
 Customer = []
 
-def submit_data():
-        if len(Customer) >= MAX_STOCK_FOR_EACH_PRODUCT_AVAILABLE:
-            return messagebox.showerror("Sorry we are completely sold out of this product!")
+#------------------------------------------------------------------------------------------------------------------------------------------------------
+# This is my name input for the program
+
+title_label = tk.Label(root, text="Please enter your information:", font=("sans-serif", 14, "bold"), bg="grey")
+title_label.grid(row=0, column=0, columnspan=4, pady=10)
+tk.Label(root, text="Name: ", fg="black", bg="grey", font=("sans-serif", 14, "bold")).grid(row=2, column=1, padx=10, pady=5)
+name_entry = tk.Entry(root, bg="darkgrey", width=15)
+name_entry.grid(row=3, column=1, padx=10, pady=5)
+name_label = tk.Label(root, text="", fg="Blue", font=("sans-serif", 12), bg="grey")
+name_label.grid(row=10, column=1, padx=10, pady=2)
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------
+
+root.mainloop()
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------
