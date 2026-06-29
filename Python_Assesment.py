1e17219a77c8e96364b2c43e577624c5ed662a07
TylerTalbot <tt23370@mbc.school.nz>
2026-06-29 12:38:02 +1200

    More completed

--- Python_Assesment.py
+++ Python_Assesment.py
@@ -37,7 +37,7 @@ Customer = []
 #------------------------------------------------------------------------------------------------------------------------------------------------------
 # This is my name input for the program
 
-TitleLabel = tk.Label(root, text="Please enter your information:", font=("sans-serif", 14, "bold"), bg="grey")
+TitleLabel = tk.Label(root, text="Please enter your information: ", font=("sans-serif", 14, "bold"), bg="grey")
 TitleLabel.grid(row=0, column=0, columnspan=4, pady=10)
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -56,6 +56,8 @@ NameLabel = tk.Label(root, text="", fg="grey", font=("sans-serif", 12), bg="grey
 NameLabel.grid(row=10, column=1, padx=10, pady=2)
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------
+# This is the input for the user to input their last Name
+# I did this so the information provided could be more accurate
 
 tk.Label(root, text="Last Name: ", fg="black", bg="grey", font=("sans-serif", 14, "bold")).grid(row=2, column=2, padx=10, pady=5)
 LastNameEntry = tk.Entry(root, bg="darkgrey", width=15)
@@ -74,6 +76,7 @@ ItemLabel.grid(row=11, column=1, padx=10, pady=2)
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------
 # this is my drop down menu for the user to select what products they want to purchace
+# When coding this part I acidendtly forgot to esnure there was a state: readoly, because it makes it so no one could type anything on the drop down menu
 
 tk.Label(root, text="Select Item: ", fg="black", bg="grey", font=("sans-serif", 14, "bold")).grid(row=4, column=2, padx=10, pady=5)
 ItemDropdown = ttk.Combobox(root, values=list(PRICING.keys()), state="readonly", width=15)
@@ -83,23 +86,24 @@ ItemDropdown.current(0)
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
 # This is the part of my code that handles the user's entries and downloads it as a txt file!
 
-def submit_order():
+def Submit():
     Name = NameEntry.get()
     LastName = LastNameEntry.get()
     Dropdown = ItemDropdown.get()
     Quantity = ItemEntry.get()
-    with open("receipt.txt", "w") as file:
-        file.write("--- RECEIPT ---\n")
+    with open("receipt.txt") as file:
+        file.write(".....RECEIPT..... \n")
         file.write(f"Customer Name: {Name} {LastName}\n")
         file.write(f"Item Selected: {Dropdown}\n")
         file.write(f"Item Quantity: {Quantity}\n")
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------
+# This is the code for the submit button, and the download the reciptt button
 
-submit_btn = tk.Button(root, text="Submit Order", bg="darkgrey", font=("sans-serif", 12, "bold"), command=submit_order)
-submit_btn.grid(row=6, column=1, columnspan=2, pady=15)
-receipt_btn = tk.Button(root, text="Download Receipt", bg="darkgrey", font=("sans-serif", 12, "bold"))
-receipt_btn.grid(row=7, column=1, columnspan=2, pady=5)
+Submit = tk.Button(root, text="Submit Order", bg="darkgrey", font=("sans-serif", 12, "bold"), command=Submit)
+Submit.grid(row=6, column=1, columnspan=2, pady=15)
+Receipt = tk.Button(root, text="Download Receipt", bg="darkgrey", font=("sans-serif", 12, "bold"))
+Receipt.grid(row=7, column=1, columnspan=2, pady=5)
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------
 # this is the end of the code, where I place my root.mainloop to conclude the code!
