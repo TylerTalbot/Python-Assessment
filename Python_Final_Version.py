63ffeae05e7ad25aa6bfadbae3c8e3f45c52d404
TylerTalbot <tt23370@mbc.school.nz>
2026-07-01 09:11:32 +1200

    Python_Final_Version: Added the dictionay and list

--- Python_Final_Version.py
+++ Python_Final_Version.py
@@ -1,3 +1,25 @@
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
+
+root = tk.Tk()
+root.geometry("400x400")
+root.grid_columnconfigure(2, weight=1)
+root.title("Party Hire Shop")
+root.configure(bg='orange')
+
+PRICING = {
+    #Furniture
+    "Chairs": 2, "Tables": 5,
+    #Tableware
+    "Plates": 3, "Cups": 1, "Napkins": 1,
+    #Cutlery
+    "Forks": 1, "Knives": 1, "Spoons": 1,
+    #Audio/Visual
+    "Speakers": 10, "Projectors": 10, "Microphones": 10,
+    #Entertainment
+    "Bouncy Castles": 20, "Clowns": 15, "Magicians": 15
+}
+
+MAX_STOCK_FOR_EACH_PRODUCT_AVAILABLE = 500
+Customer = []
